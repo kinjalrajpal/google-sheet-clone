412fc948181058f473c547ebf1214846b0b57509
kinjalrajpal <kinjalrajpal2002@gmail.com>
2025-02-25 10:55:00 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// //add cells to head-row
+// //27(no of columns) times
+// let columns = 27;
+
+// let headRow = document.querySelector(".head-row");
+
+// for(let i=0;i<columns;i++){
+//     let headCell = document.createElement("div");
+//     //65 to 90
+//     headCell.textContent = String.fromCharCode(i+65);
+//     //65
+//     headRow.appendChild(headCell);
+// }
